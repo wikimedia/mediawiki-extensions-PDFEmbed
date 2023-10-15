cb0fa38edbbe183f414cdf2c9fcea074b25f3a26
Mark A. Hershberger <mah@nichework.com>
2023-10-14 21:37:47 -0400

    More standardization

---
+++ .phan/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['file_list'] = array_merge(
+	$cfg['file_list'],
+	[
+		'PDFEmbed.php',
+		'PDFEmbed.php',
+	]
+);
+
+return $cfg;
