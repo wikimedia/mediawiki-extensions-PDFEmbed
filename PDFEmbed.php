672cad412e48627cfa4941cff9b65eef6cc43232
Mark A. Hershberger <mah@nichework.com>
2023-10-15 19:28:52 -0400

    Set up psalm and address the issues it found.

--- PDFEmbed.php
+++ PDFEmbed.php
@@ -14,6 +14,9 @@
 use MediaWiki\MediaWikiServices;
 use MediaWiki\User\UserIdentity;
 
+/**
+ * @psalm-api
+ */
 class PDFEmbed {
 
 	/**
@@ -32,13 +35,14 @@ class PDFEmbed {
 	 * Determines if the user is allowed to embed PDFs
 	 * Throws an exception that will be put in the wikitext if there is an error.
 	 *
-	 * @return true
+	 * @return void
 	 */
-	private static function handleUser( Parser $parser ): bool {
+	private static function handleUser( Parser $parser ): void {
 		$ctx = RequestContext::getMain();
 		// check the action which triggered us
 		$requestAction = $ctx->getRequest()->getVal( 'action' );
 		$revUserName = null;
+		$user = null;
 
 		if ( $requestAction === null ) {
 			$revUserName = $parser->getRevisionUser();
@@ -46,7 +50,7 @@ class PDFEmbed {
 
 		// RevUser will be null during parser tests
 		if ( $revUserName === null && $requestAction === null ) {
-			return true;
+			return;
 		}
 
 		if ( $revUserName !== null ) {
@@ -66,8 +70,67 @@ class PDFEmbed {
 			$parser->addTrackingCategory( "pdfembed-permission-problem-category" );
 
 			throw new Exception(
-				wfMessage( 'embed_pdf_no_permission', wfMessage( 'right-embed_pdf' ) )
+				wfMessage( 'embed_pdf_no_permission', wfMessage( 'right-embed_pdf' ) )->plain()
 			);
+		}
+	}
+
+	/**
+	 * Retrieve a host list from the configuration.
+	 *
+	 * @param string $type of list to get
+	 * @return array
+	 */
+	private static function getHostList( string $type ): array {
+		/** @var array {
+		 *     white: string[],
+		 *     black: string[]
+		 * }
+		 */
+		global $wgPDF;
+
+		$ret = [];
+		/**
+		 * We're checking user input here.
+		 *
+		 * @psalm-suppress RedundantConditionGivenDocblockType
+		 */
+		if ( is_array( $wgPDF[$type] ) ) {
+			$ret = $wgPDF[$type];
+		}
+		if ( count( $ret ) > 0 ) {
+			$ret = array_flip( array_map( 'strtolower', $ret ) );
+		}
+		return $ret;
+	}
+
+	/**
+	 * Check if host is on blocked list.
+	 *
+	 * @param string $host to check
+	 * @return bool
+	 */
+	private static function isHostDenied( string $host ): bool {
+		$deny = self::getHostList( "black" );
+
+		if ( isset( $deny[ $host ] ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Check if host is on allowed list.
+	 *
+	 * @param string $host to check
+	 * @return bool
+	 */
+	private static function isHostAllowed( string $host ): bool {
+		$allow = self::getHostList( "white" );
+
+		if ( !isset( $allow[ $host ] ) && count( $allow ) > 0 ) {
+			return false;
 		}
 
 		return true;
@@ -78,32 +141,33 @@ class PDFEmbed {
 	 * (and allow) lists and throws an error if the host does (not) match.
 	 *
 	 * @param string $url to check
-	 * @return array (empty if $url is not a url with a hostname in it)
+	 * @return ?array
+	 * @psalm-return ?list{ string, int }
 	 */
-	private static function maybeURL( string $url ): array {
-		global $wgPDF;
-
+	private static function maybeURL( string $url ): ?array {
 		// parse the given url
 		$parsed = parse_url( $url );
 		if ( $parsed === false || !isset( $parsed['host'] ) ) {
-			return [];
+			return null;
 		}
 		$host = strtolower( $parsed['host'] );
 		$page = intval( $parsed['fragment'] ?? 1 );
 
-		if ( isset( $wgPDF ) ) {
-			$deny = array_flip( array_map( 'strtolower', $wgPDF['black'] ?? [] ) );
-			$allow = array_flip( array_map( 'strtolower', $wgPDF['white'] ?? [] ) );
-
-			if ( !isset( $allow[ $host ] ) && count( $allow ) > 0 ) {
-				throw new Exception( wfMessage( "embed_pdf_domain_not_white", $host ) );
-			}
-
-			if ( isset( $deny[ $host ] ) ) {
-				throw new Exception( wfMessage( "embed_pdf_domain_black", $host ) );
-			}
+		if ( self::isHostDenied( $host ) ) {
+			throw new Exception(
+				wfMessage( "embed_pdf_domain_black", $host )->plain()
+			);
 		}
 
+		if ( !self::isHostAllowed( $host ) ) {
+			throw new Exception(
+				wfMessage( "embed_pdf_domain_not_white", $host )->plain()
+			);
+		}
+
+		if ( $page === 0 ) {
+			$page = 1;
+		}
 		return [ $url, $page ];
 	}
 
@@ -112,19 +176,20 @@ class PDFEmbed {
 	 *
 	 * @param string $name of upload
 	 * @return array
+	 * @psalm-return list{ string, int }
 	 */
 	private static function handleName( string $name ): array {
 		$page = 1;
 		$title = Title::newfromText( $name, NS_FILE );
 		if ( $title === null || $title->getNamespace() !== NS_FILE ) {
 			throw new Exception(
-				wfMessage( 'embed_pdf_invalid_file_name', $name )
+				wfMessage( 'embed_pdf_invalid_file_name', $name )->plain()
 			);
 		}
 
 		if ( !$title->exists() ) {
 			throw new Exception(
-				wfMessage( 'embed_pdf_invalid_file', $title->getDBkey() )
+				wfMessage( 'embed_pdf_invalid_file', $title->getDBkey() )->plain()
 			);
 		}
 
@@ -135,45 +200,14 @@ class PDFEmbed {
 		$repo = MediaWikiServices::getInstance()->getRepoGroup()->getLocalRepo();
 		$file = $repo->newFile( $title->getDBkey() );
 		if ( $file === null ) {
-			throw new Exception( wfMessage( 'embed_pdf_internal_error' ) );
+			throw new Exception( wfMessage( 'embed_pdf_internal_error' )->plain() );
 		}
 		$url = $file->getUrl();
 
+		if ( $page === 0 ) {
+			$page = 1;
+		}
 		return [ $url, $page ];
-	}
-
-	/**
-	 * Parse the arguments on the <pdf>
-	 *
-	 * @param array $args passed in
-	 * @return array
-	 */
-	private static function parseArgs( array $args ): array {
-		global $wgPdfEmbed;
-		$width = $wgPdfEmbed['width'] ?? null;
-		$height = $wgPdfEmbed['height'] ?? null;
-		$page = 0;
-		$iframe = $wgPdfEmbed['iframe'] ?? false;
-
-		if ( isset( $args['width'] ) ) {
-			$width = intval( $args['width'] );
-		}
-
-		if ( isset( $args['height'] ) ) {
-			$height = intval( $args['height'] );
-		}
-
-		if ( isset( $args['iframe'] ) ) {
-			$useFrame = strtolower( $args['iframe'] );
-			$useFrameVal = abs( intval( $args['iframe'] ) );
-			$iframe = $useFrameVal > 0 || $useFrame === "yes" || $useFrame === "true";
-		}
-
-		if ( isset( $args['page'] ) ) {
-			$page = intval( $args['page'] );
-		}
-
-		return [ $height, $width, $page, $iframe ];
 	}
 
 	/**
@@ -182,13 +216,70 @@ class PDFEmbed {
 	 *
 	 * @param string $body to check
 	 * @return array
+	 * @psalm-return list{ 0: string, 1: int }
 	 */
 	private static function handleBody( string $body ): array {
 		$parsed = self::maybeURL( $body );
-		if ( $parsed === [] ) {
+		if ( $parsed === null ) {
 			$parsed = self::handleName( $body );
 		}
 		return $parsed;
+	}
+
+	/**
+	 * Parse the arguments on the <pdf>
+	 *
+	 * @param array $arg passed in
+	 * @psalm-param $arg array(
+	 *     width: int,
+	 *     height: int,
+	 *     page: int,
+	 *     iframe: mixed
+	 * }
+	 * @return array
+	 * @psalm-return list{ int|null, int|null, int, bool }
+	 */
+	private static function parseArgs( array $arg ): array {
+		/** @var array {
+		 *     width: int,
+		 *     height: int,
+		 *     page: int,
+		 *     iframe: mixed
+		 * }
+		 */
+		global $wgPdfEmbed;
+		/** @var string|null */
+		$width = $arg['width'] ?? $wgPdfEmbed['width'] ?? null;
+		/** @var string|null */
+		$height = $arg['height'] ?? $wgPdfEmbed['height'] ?? null;
+		/** @var string|int|null */
+		$page = $arg['page'] ?? $wgPdfEmbed['page'] ?? 1;
+		/** @var string|null */
+		$iframe = $arg['iframe'] ?? $wgPdfEmbed['iframe'] ?? null;
+
+		if ( $width !== null ) {
+			$width = intval( $width );
+		}
+
+		if ( $height !== null ) {
+			$height = intval( $height );
+		}
+
+		if ( $iframe !== null ) {
+			$useFrame = strtolower( $iframe );
+			$useFrameVal = abs( intval( $iframe ) );
+			$iframe = $useFrameVal > 0 || $useFrame === "yes" || $useFrame === "true";
+		}
+
+		if ( $iframe === null ) {
+			$iframe = false;
+		}
+
+		if ( !is_int( $page ) ) {
+			$page = intval( $page );
+		}
+
+		return [ $height, $width, $page, $iframe ];
 	}
 
 	/**
@@ -200,8 +291,6 @@ class PDFEmbed {
 	 * @return string HTML
 	 */
 	public static function generateTag( $body, $args, Parser $parser ): string {
-		[ $url, $width, $height, $page, $iframe ]
-			= [ null, null, null, 1, false ];
 		try {
 			self::handleUser( $parser );
 			$parsedBody = self::handleBody( $body );
@@ -213,23 +302,25 @@ class PDFEmbed {
 			if ( $pageArg > 1 ) {
 				$page = $pageArg;
 			}
+			return self::embed( $url, $width, $height, $page, $iframe );
 		} catch ( Exception $e ) {
 			return self::error( $e->getMessage() );
 		}
-		return self::embed( $url, $width, $height, $page, $iframe );
 	}
 
 	/**
 	 * Returns an HTML node for the given file as string.
 	 *
 	 * @param string $url URL url to embed.
-	 * @param int $width of the iframe.
-	 * @param int $height of the iframe.
-	 * @param int $page of the pdf file.
+	 * @param ?int $width of the iframe.
+	 * @param ?int $height of the iframe.
+	 * @param ?int $page of the pdf file.
 	 * @param bool $iframe if an <iframe> should be returned else an <object> is returned
 	 * @return string HTML code for iframe.
 	 */
-	private static function embed( $url, $width, $height, $page, $iframe ) {
+	private static function embed(
+		string $url, ?int $width, ?int $height, ?int $page, bool $iframe
+	) {
 		# secure and concatenate the url
 		$pdfUrl = "$url#page=$page";
 		# check the embed mode and return a proper HTML element
@@ -266,7 +357,7 @@ class PDFEmbed {
 	 * @param string $error Error message to display.
 	 * @return string HTML error message.
 	 */
-	private static function error( $error ) {
+	private static function error( string $error ): string {
 		return Xml::span( $error, 'error' );
 	}
 }
